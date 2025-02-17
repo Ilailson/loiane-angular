ff9b4ec297d8817e899921f5c819ebaf6f6dadb2
ilailsonrocha <ilailsonrocha@iec.gov.br>
2025-02-17 14:51:45 -0300

    Botão crud. New com rotas

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,9 +1,11 @@
+// 2º
 import { Component, OnInit } from '@angular/core';
 import { Course } from '../model/course';
 import { CoursesService } from './services/courses.service';
 import { catchError, Observable, of } from 'rxjs';
 import { MatDialog } from '@angular/material/dialog';
 import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
+import { ActivatedRoute, Router } from '@angular/router';
 
 
 @Component({
@@ -15,9 +17,15 @@ export class CoursesComponent implements OnInit {
 
   courses$: Observable<Course[]> | undefined;
 
-  displayedColumns = ['_id', 'name', 'category'];
+                                         //acitions
+  displayedColumns = ['name', 'category', 'actions'];
 
-  constructor(private coursesService: CoursesService, public dialog: MatDialog) {}
+  constructor(
+    private coursesService: CoursesService,
+    public dialog: MatDialog,
+    private router: Router,
+    private route: ActivatedRoute,
+   ) {}
 
   ngOnInit(): void {
     this.loadCourses();
@@ -36,6 +44,10 @@ export class CoursesComponent implements OnInit {
     this.dialog.open(ErrorDialogComponent, {
       data: errorMsg
     });
+  }
+
+  onAdd() {
+    this.router.navigate(['new'], { relativeTo: this.route });
   }
 
 }
